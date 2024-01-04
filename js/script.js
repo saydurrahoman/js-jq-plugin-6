c9b7440a1e41e09926915a62dc60bdb27fc2ce42
md saydur rahoman <145344840+saydurrahoman@users.noreply.github.com>
2024-01-04 03:44:32 -1200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// javascrit
+// wow js
+wow = new WOW(
+  {
+  boxClass:     'wow',      // default
+  animateClass: 'animated', // default
+  offset:       0,          // default
+  mobile:       true,       // default
+  live:         true        // default
+})
+wow.init();
+
+
+
+
+$(document).ready(function(){
+  
+})
